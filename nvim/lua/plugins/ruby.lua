2050305577949fa930310a91e7ad89ae1f00d8b5
Mads Ulrik Svendsen <mads@sheer.dk>
2024-12-14 21:07:15 +0100

    Added my own rails plugin

---
+++ nvim/lua/plugins/ruby.lua
@@ -0,0 +1,19 @@
+return {
+  dir = "~/Documents/projects/rails.nvim",
+  opts = {},
+  config = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "ruby",
+      callback = function()
+        require("which-key").add({
+          {"<localleader>f", group="find"}
+        })
+        vim.keymap.set("n", "<localleader>fc", require("rails").find_controllers,
+          { desc = "Rails find Controllers", noremap = true, silent = true, buffer = true })
+
+      end,
+
+    })
+  end,
+
+}
